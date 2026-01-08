ff3f82ce9dca948698b5322e32f5922a1a1a334f
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-01-08 21:33:28 +0000

    Added : string.h and sys/types.h

--- shell.h
+++ shell.h
@@ -1,9 +1,11 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
 
